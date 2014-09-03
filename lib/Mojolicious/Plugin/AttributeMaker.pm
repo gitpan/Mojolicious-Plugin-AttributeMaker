f9e95a9038d40bef3cc98ba339941e487a37ae85
Evgeniy ☢ Vansevich <dev@just4i.ru>
2014-09-03 22:41:41 +0000

    Import of HAMMER/Mojolicious-Plugin-AttributeMaker-0.05 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-AttributeMaker
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         HAMMER/Mojolicious-Plugin-AttributeMaker-0.05.tar.gz
    gitpan-cpan-author:       HAMMER
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/AttributeMaker.pm
+++ lib/Mojolicious/Plugin/AttributeMaker.pm
@@ -13,11 +13,11 @@ Mojolicious::Plugin::AttributeMaker - Make attributes for Mojolicious? - easily!
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 =head1 SYNOPSIS
 
